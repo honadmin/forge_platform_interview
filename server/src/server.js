be4f4eccf7048a6df78491ac049e9d5964a6ad64
Scott <yung-hang.chang@honeywell.com>
2020-08-12 15:47:10 -0400

    first commit

---
+++ server/src/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Welcome to Forge Platform Interview')
+})
+ 
+app.listen(8080, () => console.log('server start at port 8080'))
