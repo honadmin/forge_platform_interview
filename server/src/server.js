dd960319c7aecca1450a21c20d8e4fc276e8c8a5
Scott <yung-hang.chang@honeywell.com>
2020-08-20 14:49:07 -0400

    add body-parser

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.json())
  
 app.get('/', function (req, res) {
   res.send('Welcome to Forge Platform Interview')
